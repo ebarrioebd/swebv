205800b5ab9dfd25d9f9078a1d7cd6e6d043b353
edwin barrio <47900436+ebarrioebd@users.noreply.github.com>
2023-09-22 18:17:15 -0600

    Add files via upload

--- public/js/verificarpantalla.js
+++ public/js/verificarpantalla.js
@@ -4,12 +4,12 @@ console.log( window.innerWidth,window.innerHeight);
     // tu código aquí
     console.log('La pantalla ha cambiado de tamaño', window.innerWidth,window.innerHeight); 
     if( window.innerWidth<wmin | window.innerHeight<hmin ){
-      document.write("<h1>La herramienta esta disponible para pantallas de laptop o computadoras de escritorio..</h1><h1>" + window.innerWidth+"X"+window.innerHeight+"</h1>")
+      //document.write("<h1>La herramienta esta disponible para pantallas de laptop o computadoras de escritorio..</h1><h1>" + window.innerWidth+"X"+window.innerHeight+"</h1>")
     }else{
       console.log( window.innerWidth,window.innerHeight); 
     }
 });
   if( window.innerWidth<wmin | window.innerHeight<hmin){
     console.log("Pantalla pequena")
-    document.body.innerHTML="<h1>La herramienta esta disponible para pantallas de laptop o computadoras de escritorio..</h1><h1>" + window.innerWidth+"X"+window.innerHeight+"</h1>"
+    //document.body.innerHTML="<h1>La herramienta esta disponible para pantallas de laptop o computadoras de escritorio..</h1><h1>" + window.innerWidth+"X"+window.innerHeight+"</h1>"
   }
